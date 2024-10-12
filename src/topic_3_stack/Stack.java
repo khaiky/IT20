ed3ba7c6976f6bceb3c4e2168677da6ab9a448c2
khaiky <samanthagabitano015@gmail.com>
2024-10-12 19:16:07 +0800

    added some changes

--- src/topic_3_stack/Stack.java
+++ src/topic_3_stack/Stack.java
@@ -30,4 +30,57 @@ public class Stack {
         }
         stackArray[++top] = value;
     }
+
+
+    public int peek() {
+   if (isEmpty()) {
+       System.out.println("Stack is empty. Cannot peek.");
+       return -1; 
+   }
+   return stackArray[top];
+
+
+   public class Stack {
+		private int maxSize;
+	    private int[] stackArray;
+	    private int top;
+	    
+	    public void Stack(int size) {
+	        this.maxSize = size;
+	        this.stackArray = new int[maxSize];
+	        this.top = -1; 
+	    }
+	    
+	     public boolean isEmpty() {
+	        return (top == -1);
+	    }
+
+	    public boolean isFull() {
+	        return (top == maxSize - 1);
+	    }
+	    
+	    public int size() {
+	        return top + 1;
+	    }
+	    
+	     public void push(int value) {
+	        if (isFull()) {
+	            System.out.println("Stack is full. Cannot push " + value);
+	            return;
+	        }
+	        stackArray[++top] = value;
+	    }
+	     
+	     public int peek() {
+	        if (isEmpty()) {
+	            System.out.println("Stack is empty. Cannot peek.");
+	            return -1; 
+	        }
+	        return stackArray[top];
+	    
+	     }
 }
+}
+}
+	     
+
