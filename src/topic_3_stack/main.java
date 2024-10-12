0fe550d0e3862c21625be1888cb80d0128e3f857
khaiky <samanthagabitano015@gmail.com>
2024-10-12 19:09:02 +0800

    added popped to also check if the stack is empty

--- src/topic_3_stack/main.java
+++ src/topic_3_stack/main.java
@@ -37,9 +37,14 @@ package topic_3_stack;
 				        case 3:
 				        	if(!stack.isEmpty()) {
 			         	    	 System.out.println("Top element: " + stack.peek());
-				        	
+			         	    	System.out.println("Popped element: " + stack.pop());
+				        	  } else {
+			           		   System.out.println("-----------------------");
+			           	   }
+			           	   System.out.println("---------------------------");
+			           	   break;
 }
   }
  }
-		 }
+	
 	}
