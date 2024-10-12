662904c9714a35995bc186361f9b6778ce6f2543
khaiky <samanthagabitano015@gmail.com>
2024-10-12 18:57:04 +0800

    adding scanner

---
+++ src/topic_3_stack/main.java
@@ -0,0 +1,13 @@
+package topic_3_stack;
+
+
+	import java.util.Scanner;
+	import java.util.Stack;
+
+
+	public class main {
+		 public static void main(String[] args) { 
+		        Scanner sc = new Scanner(System.in);
+		       
+}
+}
