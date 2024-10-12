1f05f9f29c7a19dbdb685d3617d159cc3125aef7
khaiky <samanthagabitano015@gmail.com>
2024-10-12 19:13:30 +0800

    added some method

--- src/topic_3_stack/Stack.java
+++ src/topic_3_stack/Stack.java
@@ -4,4 +4,30 @@ public class Stack {
 	private int maxSize;
     private int[] stackArray;
     private int top;
+    
+    public void Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1; 
+    }
+    
+     public boolean isEmpty() {
+        return (top == -1);
+    }
+
+    public boolean isFull() {
+        return (top == maxSize - 1);
+    }
+    
+    public int size() {
+        return top + 1;
+    }
+    
+     public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push " + value);
+            return;
+        }
+        stackArray[++top] = value;
+    }
 }
